7f94c5394d42e7f62470894d35aa3d482cafedf9
Zidan leo permana <zidanleopermana012@gmail.com>
2024-08-18 23:27:15 +0700

    fix some errors and add features

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 const container = document.querySelector('#container');
 let gridSize = prompt("enter Grid size:");
 
-while(gridSize > 100) {
-    gridSize = prompt("Enter grid size again!!!")
-}
+const gridInput = document.querySelector('#grid-input')
+const gridButton = document.querySelector('#grid-button')
 
-for (let i = 0; i < gridSize; i++) {
-    let boxColumn = document.createElement('div');
-    boxColumn.classList.add('boxColumn');
-    container.appendChild(boxColumn);
-    for (let j = 0; j < gridSize; j++) {
-        let box = document.createElement('div');
-        box.classList.add('box');
-        boxColumn.appendChild(box)
+function verifySize(gridSize) {
+    while (gridSize > 100 || gridSize < 3) {
+        gridSize = prompt("Enter the proper size from 3 to 100")
     }
+    return gridSize;
+}
+gridSize = verifySize(gridSize);
+setGridSize(gridSize);
+
+function setGridSize(size) {
+    for (let i = 0; i < size; i++) {
+        let boxColumn = document.createElement('div');
+        boxColumn.classList.add('boxColumn');
+        container.appendChild(boxColumn);
+        for (let j = 0; j < size; j++) {
+            let box = document.createElement('div');
+            box.classList.add('box');
+            boxColumn.appendChild(box)
+        }
+    }
+
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.addEventListener('mouseenter', e => {
+            e.target.style.background = 'blue';
+        })
+    })
 }
 
-const boxes = document.querySelectorAll('.box');
-boxes.forEach(box => {
-    box.addEventListener('mouseenter', e => {
-        e.target.style.background = 'blue';
-    })
+
+
+gridButton.addEventListener('click', () => {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+    gridSize = gridInput.value;
+    gridSize = verifySize(gridSize);
+    setGridSize(gridSize);
+    gridInput.value = "";
 })
